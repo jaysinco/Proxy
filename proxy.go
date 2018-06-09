daa187884208172541aeb0ec08a4bbc886c735e8
jaysinco <jaysinco@163.com>
2018-06-09 21:18:28 +0800

    change code format

--- proxy.go
+++ proxy.go
@@ -40,117 +40,6 @@ func main() {
 		}
 	}
 }
-func handleSocks5(conn net.Conn) {
-	closed := false
-	info <- clientConnect
-	defer func() {
-		if !closed {
-			conn.Close()
-			info <- clientClose
-		}
-	}()
-	if err := handshake(conn); err != nil {
-		return
-	}
-	remoteAddr, err := getRemoteAddr(conn)
-	if err != nil {
-		return
-	}
-	if _, err := conn.Write([]byte{0x05, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x68, 0x68}); err != nil {
-		return
-	}
-	remote, err := net.DialTimeout("tcp", remoteAddr, 20*time.Second)
-	if err != nil {
-		return
-	}
-	info <- remoteConnect
-	defer func() {
-		if !closed {
-			remote.Close()
-			info <- remoteClose
-		}
-	}()
-	done := make(chan struct{})
-	go ncopy(remote, conn, done)
-	go ncopy(conn, remote, done)
-	<-done
-	conn.Close()
-	remote.Close()
-	<-done
-	info <- clientClose
-	info <- remoteClose
-	closed = true
-}
-
-func getRemoteAddr(conn net.Conn) (string, error) {
-	buf := make([]byte, 262)
-	n, err := io.ReadAtLeast(conn, buf, 5)
-	if err != nil {
-		return "", fmt.Errorf("failed to read five two bytes")
-	}
-	if buf[0] != 0x05 {
-		return "", fmt.Errorf("wrong socks verion")
-	}
-	if buf[1] != 0x01 {
-		return "", fmt.Errorf("only support command 'connect'")
-	}
-	mlen := -1
-	switch buf[3] {
-	case 0x01:
-		mlen = 4 + net.IPv4len + 2
-	case 0x03:
-		mlen = 4 + 1 + int(buf[4]) + 2
-	case 0x04:
-		mlen = 4 + net.IPv6len + 2
-	default:
-		return "", fmt.Errorf("wrong remote address type")
-	}
-	switch {
-	case n > mlen:
-		return "", fmt.Errorf("more bytes than expected")
-	case n < mlen:
-		if _, err = io.ReadFull(conn, buf[n:mlen]); err != nil {
-			return "", fmt.Errorf("read rest: %v", err)
-		}
-	}
-	host := ""
-	switch buf[3] {
-	case 0x01:
-		host = net.IP(buf[4 : 4+net.IPv4len]).String()
-	case 0x03:
-		host = string(buf[5 : 5+int(buf[4])])
-	case 0x04:
-		host = net.IP(buf[4 : 4+net.IPv6len]).String()
-	}
-	port := binary.BigEndian.Uint16(buf[mlen-2 : mlen])
-	host = net.JoinHostPort(host, strconv.Itoa(int(port)))
-	return host, nil
-}
-
-func handshake(conn net.Conn) error {
-	buf := make([]byte, 257)
-	n, err := io.ReadAtLeast(conn, buf, 2)
-	if err != nil {
-		return fmt.Errorf("failed to read first two bytes")
-	}
-	if buf[0] != 0x05 {
-		return fmt.Errorf("wrong socks verion")
-	}
-	nmethods := int(buf[1])
-	mlen := nmethods + 2
-	switch {
-	case n > mlen:
-		return fmt.Errorf("more bytes than expected")
-	case n < mlen:
-		if _, err = io.ReadFull(conn, buf[n:mlen]); err != nil {
-			return fmt.Errorf("read rest: %v", err)
-		}
-	}
-	if _, err := conn.Write([]byte{0x05, 0x00}); err != nil {
-		return fmt.Errorf("reply: %v", err)
-	}
-	return nil
-}
 
 func handleHTTP(conn net.Conn) {
 	closed := false
@@ -206,6 +95,118 @@ func handleHTTP(conn net.Conn) {
 	info <- clientClose
 	info <- remoteClose
 	closed = true
+}
+
+func handleSocks5(conn net.Conn) {
+	closed := false
+	info <- clientConnect
+	defer func() {
+		if !closed {
+			conn.Close()
+			info <- clientClose
+		}
+	}()
+	if err := handshakeSs5(conn); err != nil {
+		return
+	}
+	remoteAddr, err := remoteAddrSs5(conn)
+	if err != nil {
+		return
+	}
+	if _, err := conn.Write([]byte{0x05, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x68, 0x68}); err != nil {
+		return
+	}
+	remote, err := net.DialTimeout("tcp", remoteAddr, 20*time.Second)
+	if err != nil {
+		return
+	}
+	info <- remoteConnect
+	defer func() {
+		if !closed {
+			remote.Close()
+			info <- remoteClose
+		}
+	}()
+	done := make(chan struct{})
+	go ncopy(remote, conn, done)
+	go ncopy(conn, remote, done)
+	<-done
+	conn.Close()
+	remote.Close()
+	<-done
+	info <- clientClose
+	info <- remoteClose
+	closed = true
+}
+
+func handshakeSs5(conn net.Conn) error {
+	buf := make([]byte, 257)
+	n, err := io.ReadAtLeast(conn, buf, 2)
+	if err != nil {
+		return fmt.Errorf("failed to read first two bytes")
+	}
+	if buf[0] != 0x05 {
+		return fmt.Errorf("wrong socks verion")
+	}
+	nmethods := int(buf[1])
+	mlen := nmethods + 2
+	switch {
+	case n > mlen:
+		return fmt.Errorf("more bytes than expected")
+	case n < mlen:
+		if _, err = io.ReadFull(conn, buf[n:mlen]); err != nil {
+			return fmt.Errorf("read rest: %v", err)
+		}
+	}
+	if _, err := conn.Write([]byte{0x05, 0x00}); err != nil {
+		return fmt.Errorf("reply: %v", err)
+	}
+	return nil
+}
+
+func remoteAddrSs5(conn net.Conn) (string, error) {
+	buf := make([]byte, 262)
+	n, err := io.ReadAtLeast(conn, buf, 5)
+	if err != nil {
+		return "", fmt.Errorf("failed to read five two bytes")
+	}
+	if buf[0] != 0x05 {
+		return "", fmt.Errorf("wrong socks verion")
+	}
+	if buf[1] != 0x01 {
+		return "", fmt.Errorf("only support command 'connect'")
+	}
+	mlen := -1
+	switch buf[3] {
+	case 0x01:
+		mlen = 4 + net.IPv4len + 2
+	case 0x03:
+		mlen = 4 + 1 + int(buf[4]) + 2
+	case 0x04:
+		mlen = 4 + net.IPv6len + 2
+	default:
+		return "", fmt.Errorf("wrong remote address type")
+	}
+	switch {
+	case n > mlen:
+		return "", fmt.Errorf("more bytes than expected")
+	case n < mlen:
+		if _, err = io.ReadFull(conn, buf[n:mlen]); err != nil {
+			return "", fmt.Errorf("read rest: %v", err)
+		}
+	}
+	host := ""
+	switch buf[3] {
+	case 0x01:
+		host = net.IP(buf[4 : 4+net.IPv4len]).String()
+	case 0x03:
+		host = string(buf[5 : 5+int(buf[4])])
+	case 0x04:
+		host = net.IP(buf[4 : 4+net.IPv6len]).String()
+	}
+	port := binary.BigEndian.Uint16(buf[mlen-2 : mlen])
+	host = net.JoinHostPort(host, strconv.Itoa(int(port)))
+	return host, nil
 }
 
 var pool = &leakyBuf{4096, make(chan []byte, 2048)}
